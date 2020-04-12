4ca2435aefa24b2e0c124de4e5bc1cbbb55c1724
Greg Skinner <changling.bob@gmail.com>
2020-04-12 17:13:57 +0100

    Wasn't exporting words, which fe expects. Might take this out later

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Manager from "./manager";
 export { default as Bucket } from "./bucket";
+export { default as Word } from "./word";
 export { DuplicateNameError, MissingBucketError } from "./errors";
 
 export default Manager;
