a84029783d7c31941801bc0f0924292da3b9286e
Greg Skinner <greg@rocketmakers.com>
2022-11-04 11:33:29 +0000

    Block 'seed' as a variable name

--- src/word/subTokens.ts
+++ src/word/subTokens.ts
@@ -1,1 +1,2 @@
-export const SUBTOKENS = ['a', 'an', 'set', 'var', 'table', 'title'];
+// These are reserved words that can't be used as variable names
+export const SUBTOKENS = ['a', 'an', 'set', 'var', 'seed', 'table', 'title'];
