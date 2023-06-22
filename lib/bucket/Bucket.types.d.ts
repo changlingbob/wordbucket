c5dff29b42ebd58864a2cebf050137a5da50dac4
Greg Skinner <changling.bob@gmail.com>
2023-06-22 18:27:39 +0100

    Move output folder

---
+++ lib/bucket/Bucket.types.d.ts
@@ -0,0 +1,3 @@
+export type Variables = {
+    [key: string]: string;
+};
--- out/utils/namespacing.js
+++
@@ -1,9 +0,0 @@
-export const splitPath = (path) => path.split('.');
-export const pathEnding = (path) => splitPath(path)[splitPath(path).length - 1];
-export const getParentFromPath = (path) => splitPath(path).slice(0, -1).join('.');
-export const pathToTuple = (path) => ({
-    parent: splitPath(path)[0],
-    child: path.split(`${splitPath(path)[0]}.`)[1],
-});
-export const tupleToPath = (tuple) => tuple.join('.');
-//# sourceMappingURL=namespacing.js.map
